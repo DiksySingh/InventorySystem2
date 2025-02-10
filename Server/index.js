ff21663bb5b1c921f0158404a7351df8c1776283
DiksySingh <singhdikshant24@gmail.com>
2025-02-10 11:31:32 +0530

    Generate Joining Form PDF  API Created

--- Server/index.js
+++ Server/index.js
@@ -41,7 +41,7 @@ app.use(cookieParser());
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true }));
 // app.use((req, res, next) => {
-//   console.log(`[${new Date().toISOString()}] ${req.method} ${req.url}`); //for show api urls
+//   console.log(`[${new Date().toISOString()}] ${req.method} ${req.url}`); //for show api urls`
 //   next();
 // });
 
--- Server/routes/commonRoutes.js
+++ Server/routes/commonRoutes.js
@@ -1,5 +1,6 @@
 const {updateLatitudeLongitude, addServicePersonState} = require("../controllers/servicePersonController");
 const {addIsActiveField} = require("../controllers/authController");
+const {generateJoiningFormPDF} = require("../helpers/generateJoiningFormPDF");
 const router = require("express").Router();
 const multer = require("multer");
 const storage = multer.memoryStorage();
@@ -8,5 +9,5 @@ const upload = multer({ storage });
 router.put("/upload-excel", upload.single('file'), updateLatitudeLongitude);
 router.put("/uploadState", upload.single('file'), addServicePersonState);
 router.put("/add-isActive-field", addIsActiveField);
-
+router.get("/generate-pdf", generateJoiningFormPDF);
 module.exports = router;
