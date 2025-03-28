831da26e347b0419cd0c0a1f227467cf9bfb9eaa
DiksySingh <singhdikshant24@gmail.com>
2025-03-28 12:51:55 +0530

    Changes in migrations

--- Server/prisma/migrations/20250328072021_inital_models/migration.sql
+++ Server/prisma/migrations/20250328072021_inital_models/migration.sql
@@ -106,35 +106,35 @@ CREATE TABLE `ManufacturingUsage` (
 -- CreateTable
 CREATE TABLE `ServiceRecord` (
     `id` VARCHAR(191) NOT NULL,
-    `itemId` VARCHAR(191) NULL,
-    `userId` VARCHAR(191) NULL,
+    `item` VARCHAR(191) NULL,
+    `subItem` VARCHAR(191) NULL,
+    `quantity` DOUBLE NULL,
+    `serialNumber` VARCHAR(191) NULL,
+    `faultAnalysis` VARCHAR(191) NULL,
+    `isRepaired` BOOLEAN NULL,
     `repairedBy` VARCHAR(191) NULL,
+    `remarks` VARCHAR(191) NULL,
+    `repairedParts` JSON NULL,
+    `userId` VARCHAR(191) NOT NULL,
     `servicedAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
-    `status` VARCHAR(191) NULL,
-    `remark` VARCHAR(191) NULL,
+    `updatedAt` DATETIME(3) NOT NULL,
 
-    INDEX `ServiceRecord_itemId_fkey`(`itemId`),
-    INDEX `ServiceRecord_userId_fkey`(`userId`),
+    INDEX `ServiceRecord_serialNumber_idx`(`serialNumber`),
+    INDEX `ServiceRecord_userId_idx`(`userId`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- CreateTable
 CREATE TABLE `ServiceUsage` (
     `id` VARCHAR(191) NOT NULL,
-    `serviceId` VARCHAR(191) NULL,
+    `serviceId` VARCHAR(191) NOT NULL,
     `rawMaterialId` VARCHAR(191) NULL,
     `quantityUsed` DOUBLE NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
 
-    INDEX `ServiceUsage_rawMaterialId_fkey`(`rawMaterialId`),
     INDEX `ServiceUsage_serviceId_fkey`(`serviceId`),
-    PRIMARY KEY (`id`)
-) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-
--- CreateTable
-CREATE TABLE `Test` (
-    `id` VARCHAR(191) NOT NULL,
-    `name` VARCHAR(191) NULL,
-
+    INDEX `ServiceUsage_rawMaterialId_fkey`(`rawMaterialId`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
@@ -163,13 +163,10 @@ ALTER TABLE `ManufacturingUsage` ADD CONSTRAINT `ManufacturingUsage_itemId_fkey`
 ALTER TABLE `ManufacturingUsage` ADD CONSTRAINT `ManufacturingUsage_rawMaterialId_fkey` FOREIGN KEY (`rawMaterialId`) REFERENCES `RawMaterial`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE `ServiceRecord` ADD CONSTRAINT `ServiceRecord_itemId_fkey` FOREIGN KEY (`itemId`) REFERENCES `Item`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE `ServiceRecord` ADD CONSTRAINT `ServiceRecord_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `User`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
+ALTER TABLE `ServiceRecord` ADD CONSTRAINT `ServiceRecord_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `User`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE `ServiceUsage` ADD CONSTRAINT `ServiceUsage_rawMaterialId_fkey` FOREIGN KEY (`rawMaterialId`) REFERENCES `RawMaterial`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE `ServiceUsage` ADD CONSTRAINT `ServiceUsage_serviceId_fkey` FOREIGN KEY (`serviceId`) REFERENCES `ServiceRecord`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
+ALTER TABLE `ServiceUsage` ADD CONSTRAINT `ServiceUsage_serviceId_fkey` FOREIGN KEY (`serviceId`) REFERENCES `ServiceRecord`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
