410facf2ed5f547957d99dd5ceb49f3ea2bdacfe
DiksySingh <singhdikshant24@gmail.com>
2025-06-26 12:26:57 +0530

    Restore service person routes for employee dashboard and new installation data

--- Server/routes/servicePersonRoutes.js
+++ Server/routes/servicePersonRoutes.js
@@ -26,11 +26,12 @@ router.post("/resend-otp", resendOtp);
 
 router.get("/service-installation-data", userVerification(['serviceperson']), getServicePersonInstallationData);
 //System Installation
-// router.get("/show-emp-dashboard", userVerification(['serviceperson', 'surveyperson']), empDashboard);
-// router.get("/show-new-install-data", userVerification(['serviceperson', 'surveyperson']), showNewInstallationDataToInstaller);
-// router.post("/update-incoming-item-status",userVerification(['serviceperson', 'surveyperson']), updateStatusOfIncomingItems);
-// router.get("/accepted-installation-data", userVerification(['serviceperson', 'surveyperson']), showAcceptedInstallationData);
-// router.post("/new-system-installation", userVerification(['serviceperson', 'surveyperson']), uploadHandler, newSystemInstallation);
+// router.get("/state-wise-service-persons",userVerification(['serviceperson', 'surveyperson']), stateWiseServiceSurveyPersons);
+router.get("/show-emp-dashboard", userVerification(['serviceperson', 'surveyperson']), empDashboard);
+router.get("/show-new-install-data", userVerification(['serviceperson', 'surveyperson']), showNewInstallationDataToInstaller);
+router.post("/update-incoming-item-status",userVerification(['serviceperson', 'surveyperson']), updateStatusOfIncomingItems);
+router.get("/accepted-installation-data", userVerification(['serviceperson', 'surveyperson']), showAcceptedInstallationData);
+router.post("/new-system-installation", userVerification(['serviceperson', 'surveyperson']), uploadHandler, newSystemInstallation);
 
 
 
