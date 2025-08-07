03e07b588781a5c891a3d78486d26c0cd13f1f0d
DiksySingh <singhdikshant24@gmail.com>
2025-08-07 12:10:22 +0530

    fix: Update user verification roles for logout and update-password routes

--- Server/routes/authRoute.js
+++ Server/routes/authRoute.js
@@ -4,8 +4,8 @@ const router = require("express").Router();
 
 router.post("/admin-signup", adminSignup);
 router.post("/login", Login);
-router.post("/logout", userVerification(["admin", "warehouseAdmin", "serviceperson"]), Logout);
-router.post("/update-password", userVerification(["serviceperson"]), updatePassword);
+router.post("/logout", userVerification(["admin", "warehouseAdmin", "serviceperson", "installer", "fieldsales", "filing"]), Logout);
+router.post("/update-password", userVerification(["warehouseAdmin", "serviceperson", "installer", "fieldsales", "filing"]), updatePassword);
 router.post("/validateRefreshToken", validateRefreshToken);
 
 module.exports = router;
