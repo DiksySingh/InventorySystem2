24436be581eb8177ad2d326413979df6892e22bf
DiksySingh <singhdikshant24@gmail.com>
2025-08-30 18:13:53 +0530

    fix: Correct warehouse field reference in produceNewItem function

--- Server/controllers/rawMaterialItemsController/adminController.js
+++ Server/controllers/rawMaterialItemsController/adminController.js
@@ -2315,7 +2315,7 @@ const produceNewItem = async (req, res) => {
     // Mongoose - update warehouse stock
     const warehouseId = "67446a8b27dae6f7f4d985dd";
     const warehouseItemsData = await WarehouseItems.findOne({
-     warehouseId: new mongoose.Types.ObjectId(warehouseId)
+      warehouse: new mongoose.Types.ObjectId(warehouseId), // ✅ correct field
     }).session(session);
 
     if (!warehouseItemsData) {
