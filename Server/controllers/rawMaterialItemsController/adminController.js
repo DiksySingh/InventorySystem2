15963308c145a06b29cd8e1c55ba0d3e00fe050a
DiksySingh <singhdikshant24@gmail.com>
2025-08-30 18:08:37 +0530

    fix: Correct warehouse ID query in produceNewItem function to use ObjectId

--- Server/controllers/rawMaterialItemsController/adminController.js
+++ Server/controllers/rawMaterialItemsController/adminController.js
@@ -2315,7 +2315,7 @@ const produceNewItem = async (req, res) => {
     // Mongoose - update warehouse stock
     const warehouseId = "67446a8b27dae6f7f4d985dd";
     const warehouseItemsData = await WarehouseItems.findOne({
-      warehouse: warehouseId,
+     warehouseId: new mongoose.Types.ObjectId(warehouseId)
     }).session(session);
 
     if (!warehouseItemsData) {
