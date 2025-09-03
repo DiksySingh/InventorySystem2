d789900bb16f45e83e605fe0557e1cc7f822e925
DiksySingh <singhdikshant24@gmail.com>
2025-09-03 11:43:34 +0530

    fix: Correct MongoDB connection variable usage in index.js

--- Server/index.js
+++ Server/index.js
@@ -25,12 +25,12 @@ const storekeeperRouter = require("./routes/rawMaterialItemsRoutes/storekeeperRo
 const testRouter = require("./routes/test");
 
 // Load environment variables
-// const MONGODB_URL = process.env.MONGODB_URL;
-const MONGO_URL = process.env.MONGO_URL;
+const MONGODB_URL = process.env.MONGODB_URL;
+// const MONGO_URL = process.env.MONGO_URL;
 const PORT = process.env.PORT || 8001;
 
 // MongoDB connection
-mongoose.connect(MONGO_URL, {
+mongoose.connect(MONGODB_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 })
