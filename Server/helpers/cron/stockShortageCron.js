d57d610ff4bf00aee5fb754757455e5289fd6f26
DiksySingh <singhdikshant24@gmail.com>
2025-12-30 10:06:11 +0530

    fix: Update cron schedule time for stock shortage report

--- Server/helpers/cron/stockShortageCron.js
+++ Server/helpers/cron/stockShortageCron.js
@@ -2,7 +2,7 @@ const cron = require("node-cron");
 const {sendAllSystemStockShortageReport} = require("../../controllers/rawMaterialItemsController/commonController");
 
 cron.schedule(
-  "31 17 * * *",
+  "10 10 * * *",
   async () => {
     console.log("⏰ Running system stock shortage cron (IST): ", new Date().toLocaleString("en-IN"));
     await sendAllSystemStockShortageReport();
