5ab0d31b561e1adc4f603eea17e4724d47f56a1b
DiksySingh <singhdikshant24@gmail.com>
2026-01-03 11:04:02 +0530

    fix: Update cron schedule for system stock shortage report to run at 11:10 AM IST

--- Server/helpers/cron/stockShortageCron.js
+++ Server/helpers/cron/stockShortageCron.js
@@ -8,7 +8,7 @@ const istTime = new Date(now.getTime() + 5.5 * 60 * 60 * 1000)
     .replace("Z", " IST");
 
 cron.schedule(
-  "59 10 * * *",
+  "10 11 * * *",
   async () => {
     console.log(`⏰ Running system stock shortage cron (IST): ${istTime}`);
     await sendAllSystemStockShortageReport();
