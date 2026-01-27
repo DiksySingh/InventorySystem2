6cd78647c14f7128ac685a4b55d5f9fd230bee70
DiksySingh <singhdikshant24@gmail.com>
2026-01-27 14:57:13 +0530

    fix: Update password added to Admin, Verification and Accounts Dept

--- Server/routes/rawMaterialItemsRoutes/userRouter.js
+++ Server/routes/rawMaterialItemsRoutes/userRouter.js
@@ -3,6 +3,6 @@ const router = express.Router();
 const userController = require("../../controllers/rawMaterialItemsController/userController");
 const {tokenVerification} = require("../../middlewares/rawMaterialMiddlewares/tokenVerification");
 
-router.put("/password", tokenVerification(['Purchase']), userController.updatePassword);
+router.put("/password", tokenVerification(['Purchase', 'Admin', 'Verification', 'Accounts']), userController.updatePassword);
 
 module.exports = router;
