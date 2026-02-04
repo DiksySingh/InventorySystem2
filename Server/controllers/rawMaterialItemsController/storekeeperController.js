31aeb47d713b54a4c02d77b757e4a9e6d2212a4f
DiksySingh <singhdikshant24@gmail.com>
2026-02-04 15:53:29 +0530

    fix: Remove approval status checks in purchase order receiving function

--- Server/controllers/rawMaterialItemsController/storekeeperController.js
+++ Server/controllers/rawMaterialItemsController/storekeeperController.js
@@ -1593,13 +1593,13 @@ const purchaseOrderReceivingBill = async (req, res) => {
 
     if (!po) throw new Error("Purchase Order not found.");
 
-    if (po.approvalStatus !== "Approved") {
-      throw new Error("Cannot receive items as PO not approved by admin.")
-    }
+    // if (po.approvalStatus !== "Approved") {
+    //   throw new Error("Cannot receive items as PO not approved by admin.")
+    // }
 
-    if (po.approvalStatus === 'Rejected') {
-      throw new Error("Cannot receive items as PO is rejected by admin");
-    }
+    // if (po.approvalStatus === 'Rejected') {
+    //   throw new Error("Cannot receive items as PO is rejected by admin");
+    // }
     
     if (["Cancelled", "Received"].includes(po.status))
       throw new Error(`PO already ${po.status}.`);
